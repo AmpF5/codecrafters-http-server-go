29d8c075417fd978c33f11e0ebe28985342e9807
AmpF5 <lolc44t@gmail.com>
2025-04-14 19:27:08 +0200

    fs3

--- app/main.go
+++ app/main.go
@@ -19,7 +19,7 @@ const (
 )
 
 var httpResponseStatusCodeMap = map[HttpResponseStatusCode]string{
-	OK:       "HTTP/1.1 200 OK\r\n",
+	OK:       "HTTP/1.1 200 OK\r\n\r\n",
 	NotFound: "HTTP/1.1 404 Not Found\r\n\r\n",
 }
 
